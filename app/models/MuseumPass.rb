cf646a70781b1e2103ffdd02ad522e7bba3c8b40
Adam DePaolo <amdepaolo@gmail.com>
2022-11-10 17:03:42 -0500

    worked on reservation system

--- app/models/MuseumPass.rb
+++ app/models/MuseumPass.rb
@@ -1,10 +1,12 @@
 class MuseumPass < ActiveRecord::Base
     has_many :reservations
 
-    def reserve(name:, email:)
+    def reserve(name:, email:, check_out:)
         Reservation.create(
             name: name,
             email: email,
+            check_out: check_out,
+            check_in: check_out.next_day(2),
             museum_pass_id: self.id
         )
     end
