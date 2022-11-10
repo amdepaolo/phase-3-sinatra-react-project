5523315794b6cdb468c8f80374dcb4eb088d3083
Adam DePaolo <amdepaolo@gmail.com>
2022-11-10 16:36:06 -0500

    began reservation method

--- app/models/MuseumPass.rb
+++ app/models/MuseumPass.rb
@@ -1,3 +1,11 @@
 class MuseumPass < ActiveRecord::Base
     has_many :reservations
+
+    def reserve(name:, email:)
+        Reservation.create(
+            name: name,
+            email: email,
+            museum_pass_id: self.id
+        )
+    end
 end
