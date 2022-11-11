5b6e8e230a19d727f41fb9d11b60366be982bb93
Adam DePaolo <amdepaolo@gmail.com>
2022-11-11 13:27:40 -0500

    began work on API routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,27 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  #read list of passes: get
+  get "/passes" do
+    passes = MuseumPass.all
+    passes.to_json
+  end
+  
+ 
+  #read list of reservations per pass: get
+  get "/passes/:id" do
+    pass = MuseumPass.find(params[:id])
+    pass.to_json(include: :reservations)
+  end
+  
+  # #create new pass: post
+
+
+  #create reservation for pass: post
+  #update and change date of reservation: patch
+  #delete reservation: delete
+ 
+
+
+
 end
