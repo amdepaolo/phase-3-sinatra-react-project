dce33ca624c52f738d6336e02cf981a7c2119a23
Adam DePaolo <amdepaolo@gmail.com>
2022-12-01 16:30:02 -0500

    seed file with fake museums

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+MuseumPass.create(name: "Natural History Museum", website: "www.naturalhistorymuseum.org")
+MuseumPass.create(name: "Modern Art Museum", website: "www.modernart.com")
+MuseumPass.create(name: "Historical Society", website: "www.history.org")
+MuseumPass.create(name: "Children's Museum", website: "www.kidsmuseum.edu")
+MuseumPass.create(name: "Botanical Gardens", website: "www.gardens.org")
 
 puts "✅ Done seeding!"
